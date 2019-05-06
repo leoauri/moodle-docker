a515315d8c98f2b9df2b364067ef0affc22b1193
Leo Auri <code@leoauri.com>
2019-05-06 12:38:42 +0200

    Enable letsencrypt SSL
    Integrate certbot into the build for auto request & renew of certs.

--- assets/foreground.sh
+++ assets/foreground.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# pass environment to cron
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 service cron start
+
+# start letsencrypt certbot
+certbot --apache --non-interactive --agree-tos --no-redirect \
+    -d "$MOODLE_DOCKER_WEB_HOST" -m "$MOODLE_DOCKER_ADMIN_EMAIL"
 
 apache2-foreground
