6401817fbb49c052eddc25f4bd334845bab1324f
Leo Auri <code@leoauri.com>
2019-10-23 11:04:15 +0000

    Get xdebug running in webserver
    Hopefully just need to configure xdebug and it will work!?

--- assets/foreground.sh
+++ assets/foreground.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo 'You hear me'
+
+if [ "$MOODLE_DOCKER_XDEBUG_KEY" ]; then
+    pecl install xdebug && \
+        echo 'zend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-20160303/xdebug.so' \
+        >> /usr/local/etc/php/php.ini
+fi
+
 # pass environment to cron
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 service cron start
